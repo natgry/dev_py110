a4b8f0f63f9e7458033c2ef625437844ab262d6d
natgry <natgry@yandex.ru>
2022-12-23 22:41:51 +0300

    Update task_1_1.py
    Составить json файл содержащий список книг отсортированных в порядке убывания их популярности.

--- Lab 4/task_1_1.py
+++ Lab 4/task_1_1.py
@@ -2,6 +2,7 @@ import json
 import re
 
 BOOKS_FILE = "books.md"
+BOOKS_FILE_OUTPUT = "books.json"
 
 BOOK_REGEX = r"\s+(?P<position>\d+).\s+" \
              r"\[(?P<book>.+?)\]" \
@@ -15,9 +16,15 @@ BOOK_REGEX = r"\s+(?P<position>\d+).\s+" \
 def task():
     book_pattern = re.compile(BOOK_REGEX, re.DOTALL)  # флаг re.DOTALL описывает, что под символом точка может содержаться символ переноса строки
 
+    book_data = list()
     with open(BOOKS_FILE) as f:
         for book in book_pattern.finditer(f.read()):
+            book_data.append(book.groupdict())
             print(json.dumps(book.groupdict(), indent=4))
+
+    with open(BOOKS_FILE_OUTPUT, "w") as f:
+        json_data = sorted(book_data, key=lambda item: int(item["position"]), reverse=True)
+        json.dump(json_data, f, indent=4)
 
 
 if __name__ == '__main__':
