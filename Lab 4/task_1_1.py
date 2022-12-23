2d57882ace2bc1ba4645548227fd010c5028f981
natgry <natgry@yandex.ru>
2022-12-23 21:10:26 +0300

    Lab 4.

---
+++ Lab 4/task_1_1.py
@@ -0,0 +1,24 @@
+import json
+import re
+
+BOOKS_FILE = "books.md"
+
+BOOK_REGEX = r"\s+(?P<position>\d+).\s+" \
+             r"\[(?P<book>.+?)\]" \
+             r"\((?P<book_url>.+?)\)" \
+             r"\s+by\s+(?P<author>.+?)" \
+             r"\s+\((?P<recommended>\d{1,3}\.\d+%)\s+recommended\)" \
+             r"\s+!\[.*?\]\((?P<cover_url>.+?)\)" \
+             r"\s+\n+\"(?P<description>.+?)\"\s+\[.*?\]\(.*?\)"""  # TODO записать ругулярное выражения для поиска книги
+
+
+def task():
+    book_pattern = re.compile(BOOK_REGEX, re.DOTALL)  # флаг re.DOTALL описывает, что под символом точка может содержаться символ переноса строки
+
+    with open(BOOKS_FILE) as f:
+        for book in book_pattern.finditer(f.read()):
+            print(json.dumps(book.groupdict(), indent=4))
+
+
+if __name__ == '__main__':
+    task()
